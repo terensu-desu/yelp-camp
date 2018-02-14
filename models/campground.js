bb3516e8626bb0ee0f5b4689efae35dc14cd03a3
Terence Mangram <tmangram@gmail.com>
2018-02-14 23:35:34 +0900

    Move schema models to separate directory and files, add comments and user definitions

---
+++ models/campground.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	},
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId, //Points to _id of object
+			ref: "Comment" //Name of the model
+		}
+	]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
