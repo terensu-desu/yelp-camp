d1251cd4a8ffc428d12bb090db8a2391e95930c2
Terence Mangram <tmangram@gmail.com>
2018-02-16 20:17:54 +0900

    Add databaseurl for local and heroku use

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var indexRoutes = require('./routes/index');
 var seedDB = require('./seeder/seeds.js');
 
 var app = express();
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://terence:vesper@ds239128.mlab.com:39128/campeggi");
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
